e542c8f720f51647b2777616af708b7869e1dc2e
Kim Goetzke <kbgoetzke@gmail.com>
2024-02-20 05:36:12 +0000

    Fix category fetching in DetailViewModel

--- Listem/ViewModel/DetailViewModel.cs
+++ Listem/ViewModel/DetailViewModel.cs
@@ -10,14 +10,11 @@ namespace Listem.ViewModel;
 [QueryProperty(nameof(ObservableItem), nameof(ObservableItem))]
 public partial class DetailViewModel : ObservableObject
 {
-    [ObservableProperty]
-    private ObservableCollection<ObservableCategory> _categories = [];
+    [ObservableProperty] private ObservableCollection<ObservableCategory> _categories = [];
 
-    [ObservableProperty]
-    private ObservableItem _observableItem;
+    [ObservableProperty] private ObservableCategory _currentCategory;
 
-    [ObservableProperty]
-    private ObservableCategory _currentCategory;
+    [ObservableProperty] private ObservableItem _observableItem;
 
     private readonly ICategoryService _categoryService;
     private readonly IItemService _itemService;
@@ -41,14 +38,14 @@ public partial class DetailViewModel : ObservableObject
         ObservableItem.CategoryName = CurrentCategory.Name;
         await _itemService.CreateOrUpdateAsync(ObservableItem);
         Notifier.ShowToast($"Updated: {ObservableItem.Title}");
-        await Shell.Current.GoToAsync("..", true);
+        await Shell.Current.Navigation.PopModalAsync();
     }
 
     private async void SetCategories()
     {
-        var loadedStores = await _categoryService.GetAllAsync();
+        var loaded = await _categoryService.GetAllByListIdAsync(ObservableItem.ListId);
         Categories.Clear();
-        foreach (var category in loadedStores)
+        foreach (var category in loaded)
         {
             Categories.Add(category);
             if (category.Name == ObservableItem.CategoryName)
