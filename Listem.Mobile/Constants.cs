3cf111cd13bf768a759fb42dd37adb0e1357bb25
Kim Goetzke <kbgoetzke@gmail.com>
2024-03-18 20:45:03 +0000

    Add default error messages as const

--- Listem.Mobile/Constants.cs
+++ Listem.Mobile/Constants.cs
@@ -5,4 +5,9 @@ public static class Constants
     public const string HttpClientName = "ListemClient";
     public const string BaseUrlLocalhost = "http://10.0.2.2:5041";
     public const string User = "CurrentUser";
+
+    // Error messages
+    public const string UnauthorisedMessage = "You are not authorised to make this request";
+    public const string ForbiddenMessage = "You are not allowed to make this request";
+    public const string DefaultMessage = "Sorry, something went wrong - please try again";
 }
