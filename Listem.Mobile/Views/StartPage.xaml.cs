e9aa7af6b87673396619a4abe53b110f43fdb527
Kim Goetzke <kbgoetzke@gmail.com>
2024-03-23 08:35:33 +0000

    Fix bug where user isn't always redirected on sign in

--- Listem.Mobile/Views/StartPage.xaml.cs
+++ Listem.Mobile/Views/StartPage.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel;
-using Listem.Mobile.Utilities;
 using Listem.Mobile.ViewModel;
 
 namespace Listem.Mobile.Views;
@@ -16,12 +15,21 @@ public partial class StartPage
         BindingContext = _viewModel;
     }
 
+    // Evaluates redirect when user comes back from sign in page - is evaluated before token is refreshed on
+    // initial app startup, so we need ViewModel_PropertyChanged too; there's probably a cleaner way to do this
+    protected override void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        base.OnNavigatedTo(args);
+        _viewModel.RedirectIfUserIsSignedIn();
+    }
+
+    // Evaluates redirect on app startup - doesn't kick when user comes back from sign in page
+    // which is why we need to evaluate on OnNavigatedTo too; there's probably a cleaner way to do this
     private void ViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
     {
         if (e.PropertyName != nameof(_viewModel.IsUserSignedIn))
             return;
 
-        Logger.Log("User just signed in, redirecting to MainPage...");
         _viewModel.RedirectIfUserIsSignedIn();
     }
 
