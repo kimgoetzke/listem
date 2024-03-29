7f450a6133bbf01afb6ad4bf1433300d42967c10
Kim Goetzke <kbgoetzke@gmail.com>
2024-03-29 10:47:25 +0000

    Update LoggerExtensions

--- Listem.Mobile/Utilities/LoggerExtensions.cs
+++ Listem.Mobile/Utilities/LoggerExtensions.cs
@@ -4,21 +4,31 @@ public static class LoggerExtensions
 {
   public static void Debug(this ILogger logger, string? message, params object?[] args)
   {
-    logger.LogDebug(message, args);
+    logger.Log(LogLevel.Debug, message, args);
   }
 
   public static void Info(this ILogger logger, string? message, params object?[] args)
   {
-    logger.LogInformation(message, args);
+    logger.Log(LogLevel.Information, message, args);
+  }
+
+  public static void Info(
+    this ILogger logger,
+    Exception? exception,
+    string? message,
+    params object?[] args
+  )
+  {
+    logger.Log(LogLevel.Information, exception, message, args);
   }
 
   public static void Warn(this ILogger logger, string? message, params object?[] args)
   {
-    logger.LogWarning(message, args);
+    logger.Log(LogLevel.Warning, message, args);
   }
 
   public static void Error(this ILogger logger, string? message, params object?[] args)
   {
-    logger.LogError(message, args);
+    logger.Log(LogLevel.Error, message, args);
   }
 }
