5b6f06012d10e44d95ed1f7d50657110dc9235d9
Kim Goetzke <kbgoetzke@gmail.com>
2024-04-15 19:11:55 +0100

    Make Email not null

--- Listem.Mobile/Models/UserData.cs
+++ Listem.Mobile/Models/UserData.cs
@@ -6,12 +6,13 @@ namespace Listem.Mobile.Models;
 
 [SuppressMessage("ReSharper", "RedundantExtendsListEntry")]
 [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
+[SuppressMessage("ReSharper", "AutoPropertyCanBeMadeGetOnly.Global")]
 public partial class UserData : IRealmObject
 {
   [PrimaryKey]
   [MapTo("_id")]
   public ObjectId Id { get; set; }
-  public string Email { get; set; }
+  public string Email { get; set; } = null!;
 
   public string ToLog()
   {
