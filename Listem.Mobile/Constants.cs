8ef0380f0796c77802b5e23cec0a7aa2a04f4b9f
Kim Goetzke <kbgoetzke@gmail.com>
2025-07-13 15:21:16 +0100

    Remove unused constants

--- Listem.Mobile/Constants.cs
+++ Listem.Mobile/Constants.cs
@@ -3,12 +3,7 @@
 public static class Constants
 {
   public const string LoggerTag = "Listem";
-  public const string User = "CurrentUser";
-  public const string KnownUsers = "KnownUsers";
-  public const string LocalEncryptionKey = "LocalEncryptionKey";
   public const string DefaultCategoryName = "None";
-  public const string TokenRefreshFailedMessage =
-    "Couldn't refresh your session - please sign in again";
   public const string PrivacyPolicyUrl =
     "https://github.com/kimgoetzke/listem/blob/main/docs/privacy-policy.md";
   public const string LogFileName = "log-.txt";
